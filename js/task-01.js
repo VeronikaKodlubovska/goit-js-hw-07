8579611efc002ed000c800b4c48cc23b53510340
Veronika Kodlubovska <koldubovskaja.v95@gmail.com>
2023-12-10 22:32:22 +0200

    first day

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+
+
+let headingsArray = [];
+let nestedListsArray = [];
+
+categoriesItems.forEach(list => {
+    const headings = list.querySelectorAll('h2');
+    headingsArray.push(...headings);
+
+    const nestedLists = list.querySelector('ul');
+    if (nestedLists) {
+        nestedListsArray.push(nestedLists.children.length);
+    } else {
+        nestedListsArray.push(0);
+    }
+})
+console.log(`Number of categories: ${headingsArray.length}`);
+console.log(`Category: ${headingsArray[0].textContent}`);
+console.log(`Elements:${nestedListsArray[0]}`);
+console.log(`Category: ${headingsArray[1].textContent}`);
+console.log(`Elements:${nestedListsArray[1]}`);
+console.log(`Category: ${headingsArray[2].textContent}`);
+console.log(`Elements:${nestedListsArray[2]}`);
